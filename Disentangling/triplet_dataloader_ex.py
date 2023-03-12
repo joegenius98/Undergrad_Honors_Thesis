eefe556ba76d70329976354614b79959c49113ce
Joseph S. Lee <jslee08@email.wm.edu>
2023-03-12 19:53:31 -0400

    Implement triplet dataloader

--- Disentangling/triplet_dataloader_ex.py
+++ Disentangling/triplet_dataloader_ex.py
@@ -12,29 +12,117 @@ class ImageDataset(Dataset):
     def __getitem__(self, index):
         return self.images[index]
 
-def collate_fn(batch):
+
+
+def convert_to_tripletBatch_debug(batch):
+    """"
+    Proccesses a PyTorch-default batch of data sequentially, which (in the form) of a list,
+    to return a batch of triplets (one image, its augmentation, and another image)
+    or a (one image, its augmentation) pair 
+    
+    Keyword arguments:
+    batch -- a list of images, its length is batch_size
+    Return: Tensor with shape (batch_size, 3 or 2, num_channels, height, width)
+    """
+
     print("Here is the batch in `collate_fn`")
     print(batch)
-    batch_size = len(batch)
-    print(f"Batch size: {batch_size}")
-    images_batch = torch.zeros((batch_size-1, 3, 1,  3, 3))
-    
-    for i in range(batch_size - 1): # we'll need a batch size of at least 2 (need at least two randomly selected images)
-        first_image_index = i
-        second_image_index = i + 1 # can be i and i + 1 if we toggle shuffle = True, so that it's random
-        first_image = batch[first_image_index]
-        second_image = batch[second_image_index]
-        first_image_augmented = rotate(first_image, 180)
+    print("List of Tensor sizes in batch:")
+    print([tensor.shape for tensor in batch])
 
-        images_batch[i, 0, :, :, :] = first_image
-        images_batch[i, 1, :, :, :] = first_image_augmented
-        images_batch[i, 2, :, :, :] = second_image
+    # get image dimensions, assuming all images are the same size
+    # and it is number-of-channels (nc) first
+    nc, h, w = batch[0].shape[-3], batch[0].shape[-2], batch[0].shape[-1]
+
+    batch_size = len(batch)
+
+
+    """to return batches of 3 (original image, augmented original, and another image)
+    implies we need at least two separate images to work off of""" 
+    if batch_size >= 2:
+        print(f"Batch size: {batch_size}")
+        # format: (batch_size, 3 for (original, augmented, another), num_channels, height, width)
+        images_batch = torch.zeros((batch_size, 3, nc, h, w))
+        
+        for i in range(batch_size - 1):         
+            first_image_index = i
+            second_image_index = i + 1 # can be i and i + 1 if we toggle shuffle = True, so that it's random
+            first_image = batch[first_image_index]
+            second_image = batch[second_image_index]
+            first_image_augmented = rotate(first_image, 180)
+
+            images_batch[i, 0, :, :, :] = first_image
+            images_batch[i, 1, :, :, :] = first_image_augmented
+            images_batch[i, 2, :, :, :] = second_image
+        
+        images_batch[batch_size-1, 0, :, :, :] = batch[batch_size-1]
+        images_batch[batch_size-1, 1, :, :, :] = rotate(batch[batch_size-1], 180)
+        images_batch[batch_size-1, 2, :, :, :] = batch[0] 
+
+
+    # otherwise, just create an (original image, augmented original) pair
+    else:
+        assert batch_size == 1
+        images_batch = torch.zeros((batch_size, 2, nc, h, w))
+        images_batch[0, 0, :, :, :] = batch[0]
+        images_batch[0, 1, :, :, :] = rotate(batch[0], 180)
         
     return images_batch
 
+
+
+def convert_to_tripletBatch(batch):
+    """"
+    Proccesses a batch of data sequentially (in the form) of a list
+    to return a batch of triplets (one image, its augmentation, and another image)
+    or a (one image, its augmentation) pair 
+    
+    Keyword arguments:
+    batch -- a list of images, its length is batch_size
+    Return: Tensor with shape (batch_size, 3 or 2, num_channels, height, width)
+    """
+
+    # get image dimensions, assuming all images are the same size
+    # and it is number-of-channels (nc) first
+    nc, h, w = batch[0].shape[-3], batch[0].shape[-2], batch[0].shape[-1]
+    batch_size = len(batch)
+
+    """to return batches of 3 (original image, augmented original, and another image)
+    implies we need at least two separate images to work off of""" 
+    if batch_size >= 2:
+        print(f"Batch size: {batch_size}")
+        # format: (batch_size, 3 for (original, augmented, another), num_channels, height, width)
+        images_batch = torch.zeros((batch_size, 3, nc, h, w))
+        
+        for i in range(batch_size - 1):         
+            first_image_index = i
+            second_image_index = i + 1 # can be i and i + 1 if we toggle shuffle = True, so that it's random
+            first_image = batch[first_image_index]
+            second_image = batch[second_image_index]
+            first_image_augmented = rotate(first_image, 180)
+
+            images_batch[i, 0, :, :, :] = first_image
+            images_batch[i, 1, :, :, :] = first_image_augmented
+            images_batch[i, 2, :, :, :] = second_image
+        
+        images_batch[batch_size-1, 0, :, :, :] = batch[batch_size-1]
+        images_batch[batch_size-1, 1, :, :, :] = rotate(batch[batch_size-1], 180)
+        images_batch[batch_size-1, 2, :, :, :] = batch[0] 
+
+
+    # otherwise, just create an (original image, augmented original) pair
+    else:
+        assert batch_size == 1
+        images_batch = torch.zeros((batch_size, 2, nc, h, w))
+        images_batch[0, 0, :, :, :] = batch[0]
+        images_batch[0, 1, :, :, :] = rotate(batch[0], 180)
+        
+    return images_batch
+
+
 images = torch.randn(5, 1, 3, 3)
 dataset = ImageDataset(images)
-dataloader = DataLoader(dataset, batch_size=10, collate_fn=collate_fn, shuffle=True)
+dataloader = DataLoader(dataset, batch_size=10, collate_fn=convert_to_tripletBatch_debug, shuffle=True)
 
 for batch in dataloader:
     print(batch)
