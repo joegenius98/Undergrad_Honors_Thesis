2252a50bb3bd5940dca319b33c6aae40257b52d9
Joseph S. Lee <jslee08@wm.edu>
2023-03-21 22:33:01 -0400

    Fix run scripts

--- Disentangling/run_honors_thesis_init.sh
+++ Disentangling/run_honors_thesis_init.sh
@@ -1,7 +1,12 @@
 #! /bin/sh
 
-nice python3 main.py --train True --seed 1 --cuda False --gpu 0 --max_iter 1.5e6 --batch_size 64 \
-    --limit 3 --is_PID False --z_dim 10 --beta 4 --objective H --model H \
-    --lr 1e-4 --beta1 0.9 --beta2 0.999 --dset_dir ./data --dataset dsprites --image_size 64 \
-    --num_workers 4 --viz_on True --viz_name Honors_Thesis_First_Run --viz_port 8097 --save_output True \
-    --output_dir ./outputs --gather_step 10000 --save_step 10000 --ckpt_dir ./checkpoints 
+# To load from the last checkpoint, specify --ckpt_name last
+
+nice python main.py --train True --seed 1 --cuda False --gpu 0 --max_iter 1.5e6 --batch_size 64 \
+    --lr 1e-4 --beta1 0.9 --beta2 0.999 --z_dim 10 --model L --objective L \
+    --beta 4 --beta_TC 6 --C_tc_start 0 --C_tc_max 5 --C_tc_step_val 0.02 --lambda_tc 1 \
+    --num_sim_factors 1 --augment_factor 1 \
+    --dset_dir ./data --dataset dsprites --image_size 64 --num_workers 4 \
+    --viz_on True --viz_name Honors_Thesis_First_Run --viz_port 8097 --gather_step 10000 \
+    --save_step 10000 --save_output True --output_dir ./outputs --limit 3 \
+    --ckpt_dir ./checkpoints \
