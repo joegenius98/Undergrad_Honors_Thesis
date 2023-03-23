5e4d3c019d17a10997d4c70d79b6ab85b10e64b0
Joseph S. Lee <jslee08@email.wm.edu>
2023-03-23 08:13:14 -0400

    Rewrite  as

--- Disentangling/run_honors_thesis_init.sh
+++ Disentangling/run_honors_thesis_init.sh
@@ -23,6 +23,6 @@ nice python main.py --train True --seed 1 --cuda True --gpu 0 --max_iter 1.5e6 -
     --beta 100 --beta_TC 6 --C_tc_start 0 --C_tc_max 5 --C_tc_step_val 0.02 --lambda_tc 1 \
     --num_sim_factors 2 --augment_factor 1000 \
     --dset_dir ./data --dataset dsprites --image_size 64 --num_workers 4 \
-    --viz_on True --viz_name Honors_Thesis_Second_Run --viz_port 8097 --gather_step 10000 \
+    --viz_on True --viz_name Honors_Thesis_2nd_Run --viz_port 8097 --gather_step 10000 \
     --save_step 10000 --save_output True --output_dir ./outputs --limit 3 \
     --ckpt_dir ./checkpoints \
