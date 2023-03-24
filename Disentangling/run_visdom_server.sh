8116f05f0d881b60079c6a78040f7f64f7465b21
Joseph S. Lee <jslee08@email.wm.edu>
2023-03-23 21:51:03 -0400

    Rename .out file name for Visdom replay log script

--- Disentangling/run_visdom_server.sh
+++ Disentangling/run_visdom_server.sh
@@ -14,7 +14,7 @@ nohup python -m visdom.server &> visdom_server.out &
 
 # If a log directory is provided, replay the log using Visdom
 if [ ! -z "$log_dir" ] ; then
-  nohup python -c "import visdom; vis = visdom.Visdom(port=8097); vis.replay_log('$log_dir')" &> script.out &
+  nohup python -c "import visdom; vis = visdom.Visdom(port=8097); vis.replay_log('$log_dir')" &> vis_replay_script.out &
 fi
 
 
