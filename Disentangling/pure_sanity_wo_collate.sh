a4e21ca046cee4caefdd9babe50008c73cb52d47
Joseph S. Lee <jslee08@email.wm.edu>
2023-03-24 09:26:42 -0400

    Correct batch size to be a multiple of 3

--- Disentangling/pure_sanity_wo_collate.sh
+++ Disentangling/pure_sanity_wo_collate.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-nice python main.py --train True --seed 1 --cuda True --gpu 1 --max_iter 1.5e6 --batch_size 64 \
+nice python main.py --train True --seed 1 --cuda True --gpu 1 --max_iter 1.5e6 --batch_size 66 \
     --lr 1e-4 --beta1 0.9 --beta2 0.999 --z_dim 10 --model L --objective H \
     --beta 100 --dset_dir ./data --dataset dsprites --image_size 64 --num_workers 4 \
     --viz_on True --viz_name Honors_Thesis_Pure_BetaVAE --viz_port 8097 --gather_step 10000 \
