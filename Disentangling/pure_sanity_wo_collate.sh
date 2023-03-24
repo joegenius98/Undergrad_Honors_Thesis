1d779acb6669294d90f8a5d54c47a3d95befceef
Joseph S. Lee <jslee08@email.wm.edu>
2023-03-24 09:50:40 -0400

    Change port to avoid conflict

--- Disentangling/pure_sanity_wo_collate.sh
+++ Disentangling/pure_sanity_wo_collate.sh
@@ -3,6 +3,6 @@
 nice python main.py --train True --seed 1 --cuda True --gpu 0 --max_iter 1.5e6 --batch_size 66 \
     --lr 1e-4 --beta1 0.9 --beta2 0.999 --z_dim 10 --model L --objective H \
     --beta 100 --dset_dir ./data --dataset dsprites --image_size 64 --num_workers 4 \
-    --viz_on True --viz_name Honors_Thesis_Pure_BetaVAE --viz_port 8097 --gather_step 10000 \
+    --viz_on True --viz_name Honors_Thesis_Pure_BetaVAE --viz_port 8098 --gather_step 10000 \
     --save_step 10000 --save_output True --output_dir ./outputs --limit 3 \
     --ckpt_dir ./checkpoints \
