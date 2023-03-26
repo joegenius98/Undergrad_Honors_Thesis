35fa8a8b7cf1f802264b528e3bbd2e14005e51e0
Joseph S. Lee <jslee08@wm.edu>
2023-03-26 07:42:24 -0400

    Set up MLP experiment with thesis concepts

---
+++ beta-tcvae/run_MLP_anneal_thesis.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# beta is set to 6 to gradually increase it the whole time
+nice python vae_quant.py --dataset shapes --beta 6 \
+ --tcvae --visdom --visdom_port 4522 --mss --exclude-mutinfo --num-epochs 100 \
+ --log_freq 1200 \
+ --beta-anneal --lambda-anneal --use_augment_dataloader --augment_factor 1 --num_sim_factors 2 \
+ --save MLP_anneal_thesis
