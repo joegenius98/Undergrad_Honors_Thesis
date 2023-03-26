e84d5e776e99c6024362b1ef0919f2c2147ae40f
Joseph S. Lee <jslee08@wm.edu>
2023-03-26 06:03:42 -0400

    Specify save environments in 3 upcoming experiments

--- beta-tcvae/run_CNN_with_anneal.sh
+++ beta-tcvae/run_CNN_with_anneal.sh
@@ -2,4 +2,4 @@
 # beta is set to 6 to gradually increase it the whole time
 nice python vae_quant.py --dataset shapes --beta 6 \
  --tcvae --visdom --conv --mss --exclude-mutinfo --num-epochs 100 \
- --log_freq 1200 --beta-anneal --lambda-anneal
+ --log_freq 1200 --beta-anneal --lambda-anneal --save CNN_with_anneal
--- beta-tcvae/run_MLP_with_anneal.sh
+++ beta-tcvae/run_MLP_with_anneal.sh
@@ -2,4 +2,4 @@
 # beta is set to 6 to gradually increase it the whole time
 nice python vae_quant.py --dataset shapes --beta 6 \
  --tcvae --visdom --mss --exclude-mutinfo --num-epochs 100 \
- --log_freq 1200 --beta-anneal --lambda-anneal
+ --log_freq 1200 --beta-anneal --lambda-anneal --save MLP_with_anneal
