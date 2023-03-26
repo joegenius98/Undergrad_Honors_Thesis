8102fe11539d1218180810e16f8fc7398132363a
Joseph S. Lee <jslee08@wm.edu>
2023-03-26 05:23:41 -0400

    Add three incoming experiment scripts

---
+++ beta-tcvae/run_CNN_anneal_thesis.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# beta is set to 6 to gradually increase it the whole time
+nice python vae_quant.py --dataset shapes --beta 6 \
+ --tcvae --visdom --conv --mss --exclude-mutinfo --num-epochs 100 \
+ --log_freq 1200 --beta-anneal --lambda-anneal --augment_factor 1 --num_sim_factors 2
