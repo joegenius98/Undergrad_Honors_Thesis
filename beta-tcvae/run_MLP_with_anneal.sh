195e36849d81c9aa11b05071a1a4aeda818090f5
Joseph S. Lee <jslee08@wm.edu>
2023-03-26 06:07:40 -0400

    Specify Visdom ports for 3 upcoming experiments

--- beta-tcvae/run_MLP_with_anneal.sh
+++ beta-tcvae/run_MLP_with_anneal.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # beta is set to 6 to gradually increase it the whole time
 nice python vae_quant.py --dataset shapes --beta 6 \
- --tcvae --visdom --mss --exclude-mutinfo --num-epochs 100 \
+ --tcvae --visdom --visdom_port 4500 --mss --exclude-mutinfo --num-epochs 100 \
  --log_freq 1200 --beta-anneal --lambda-anneal --save MLP_with_anneal
