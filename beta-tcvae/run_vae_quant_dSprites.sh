d2c739d90a96ec1b1a6ea21b6bfc55cfd1ebbf25
Joseph S. Lee <jslee08@wm.edu>
2023-03-26 08:33:31 -0400

    Create script for best MIG run script + thesis concepts

--- beta-tcvae/run_vae_quant_dSprites.sh
+++ beta-tcvae/run_vae_quant_dSprites.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-nice python vae_quant.py --dataset shapes --beta 8 --tcvae --visdom --mss --exclude-mutinfo --num-epochs 100
+nice python vae_quant.py --dataset shapes --beta 8 --tcvae --visdom --mss --exclude-mutinfo --num-epochs 100 \
+ --log_freq 1200 --use_augment_dataloader --augment_factor 1 --num_sim_factors 2 --save bestMIG_with_thesis
