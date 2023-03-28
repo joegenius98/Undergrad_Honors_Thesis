ba614023985ef29c5688303958d046d6a5c885b4
Joseph S. Lee <jslee08@email.wm.edu>
2023-03-28 14:55:17 -0400

    Add results from setting k eq. to 1

--- beta-tcvae/run_bestMIG_with_thesis.sh
+++ beta-tcvae/run_bestMIG_with_thesis.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-nice python vae_quant.py --dataset shapes --beta 8 --tcvae --visdom --mss --exclude-mutinfo --num-epochs 100 \
- --log_freq 1200 --use_augment_dataloader --augment_factor 50 --num_sim_factors 2 --save bestMIG_with_thesis_aug_50
+nice python vae_quant.py --dataset shapes --beta 8 --tcvae --exclude-mutinfo --visdom --visdom_port 4500 --mss --num-epochs 100 \
+ --log_freq 1200 --use_augment_dataloader --augment_factor 100 --num_sim_factors 1 --save k_eq_1
