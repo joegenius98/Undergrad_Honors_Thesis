1fe84e7be8ac7cba57046d3c642ccad4839d413a
Joseph S. Lee <jslee08@wm.edu>
2023-03-29 01:34:04 -0400

    Change `viz_port` for FactorVAE reproduction

--- FactorVAE/scripts/run_dsprites_gamma10.sh
+++ FactorVAE/scripts/run_dsprites_gamma10.sh
@@ -2,6 +2,7 @@
 
 python main.py --dataset dsprites --num_workers 4 --batch_size 64 \
                --output_save True --viz_on True \
+               --viz_port 4521 \
                --viz_ll_iter 1000 --viz_la_iter 5000 \
                --viz_ra_iter 10000 --viz_ta_iter 10000 \
                --ckpt_save_iter 10000 --max_iter 7e5 \
