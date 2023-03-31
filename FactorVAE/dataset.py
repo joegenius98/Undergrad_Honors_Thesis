44fcfc78e88d5360b46cce876f058679c041bbc3
Joseph S. Lee <jslee08@wm.edu>
2023-03-31 17:35:24 -0400

    Make thesis dataset and collate func. work for FactorVAE

--- FactorVAE/dataset.py
+++ FactorVAE/dataset.py
@@ -55,6 +55,42 @@ class CustomTensorDataset(Dataset):
         return self.data_tensor.size(0)
 
 
+class ThesisTensorDataset(Dataset):
+    """
+    Helper dataset so that each minibatch of data
+    will be a tuple of two items of equal length:
+    one being a sequence of [img1, img1_augmented, img2, img2_augmented, ...]
+    and another being [random_img1, random_img2, ...]
+    """
+    def __init__(self, data_tensor, transform=None):
+        self.data_tensor = data_tensor
+        self.transform = transform
+        self.indices = range(len(self))
+
+    def __getitem__(self, index1):
+        index2 = random.choice(self.indices)
+        while index2 == index1: 
+            index2 = random.choice(self.indices)
+
+        index3 = random.choice(self.indices)
+        while index3 == index2 or index3 == index1: 
+            index3 = random.choice(self.indices)
+
+        img1 = self.data_tensor[index1]
+        img2 = self.data_tensor[index2]
+        img3 = self.data_tensor[index3]
+
+        if self.transform is not None:
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+            img3 = self.transform(img3)
+
+        return img1, img2, img3
+
+    def __len__(self):
+        return self.data_tensor.size(0)
+
+
 class TensorDataset(Dataset):
     def __init__(self, data_tensor):
         self.data_tensor = data_tensor
@@ -68,9 +104,7 @@ class TensorDataset(Dataset):
 
 def augmented_batch(batch):
     """"
-    Translates Joseph Lee's honors thesis idea with data augmentation --> dataloader implementation 
-
-    Proccesses a batch of data sequentially (in the form) of a list
+    Proccesses a batch of data sequentially (in the form) of an iterable
     to return a batch of pairs (1st image, 1st img. augmented, second image, 2nd img. augmented, etc.)
     
     Keyword arguments:
@@ -95,6 +129,27 @@ def augmented_batch(batch):
         images_batch[2*i+1, :, :, :] = first_image_augmented
 
     return images_batch
+
+
+def augmented_batch_2tuple(lst_3_tup_data_pts):
+    """
+    Processes a list of tuples, each with size 2 (two randomly selected data points),
+    and returns a tuple of size 2, one with the augmented batch with the first elements
+    and the other with the augmented batch with the second elements.
+    """
+    # zip does basically does the equivalent as the below commented out lines of code
+    # data_pts_to_augment = tuple(tup[0] for tup in lst_3_tup_data_pts)
+    # random_datapts_1 = tuple(tup[1] for tup in lst_3_tup_data_pts])
+    # random_datapts_2 = tuple(tup[2] for tup in lst_3_tup_data_pts)
+    data_pts_to_augment, random_datapts_1, random_datapts_2 = zip(*lst_3_tup_data_pts)
+
+    aug_batch = augmented_batch(data_pts_to_augment)
+    rand_batch = torch.stack(random_datapts_1 + random_datapts_2)
+
+    assert len(aug_batch) == len(rand_batch)
+
+    return aug_batch, rand_batch
+    
 
 
 def return_data(args):
@@ -122,7 +177,7 @@ def return_data(args):
         data = np.load(root, encoding='latin1')
         data = torch.from_numpy(data['imgs']).unsqueeze(1).float()
         train_kwargs = {'data_tensor':data}
-        dset = TensorDataset if args.use_augment_dataloader else CustomTensorDataset
+        dset = ThesisTensorDataset if args.use_augment_dataloader else CustomTensorDataset
     else:
         raise NotImplementedError
 
@@ -130,7 +185,7 @@ def return_data(args):
     train_data = dset(**train_kwargs)
     train_loader = DataLoader(train_data,
                               batch_size=batch_size,
-                              collate_fn=augmented_batch if args.use_augment_dataloader else None,
+                              collate_fn=augmented_batch_2tuple if args.use_augment_dataloader else None,
                               shuffle=True,
                               num_workers=num_workers,
                               pin_memory=True,
