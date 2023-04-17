8edacba54ea31ad342d0395c33909962e99dab0e
Joseph S. Lee <jslee08@wm.edu>
2023-04-17 14:58:46 -0400

    Get rid of `ckpt_save_iter` redundancy

--- FactorVAE/thesis_dsprites_scripts/k2/k2_af2.sh
+++ FactorVAE/thesis_dsprites_scripts/k2/k2_af2.sh
@@ -4,7 +4,7 @@ nice python main.py --seed 1 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
  --output_save True --viz_on True --viz_port 4522 \
  --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 10000 --viz_ta_iter 10000 \
- --ckpt_save_iter 10000 --max_iter 7e5 --print_iter 5000 \
+ --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
  --use_augment_dataloader --num_sim_factors 2 --augment_factor 2 \
  --name fVAE_k2_af2_seed1 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 100_000
@@ -13,7 +13,7 @@ nice python main.py --seed 2 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
  --output_save True --viz_on True --viz_port 4522 \
  --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 10000 --viz_ta_iter 10000 \
- --ckpt_save_iter 10000 --max_iter 7e5 --print_iter 5000 \
+ --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
  --use_augment_dataloader --num_sim_factors 2 --augment_factor 2 \
  --name fVAE_k2_af2_seed2 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 100_000
@@ -22,7 +22,7 @@ nice python main.py --seed 3 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
  --output_save True --viz_on True --viz_port 4522 \
  --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 10000 --viz_ta_iter 10000 \
- --ckpt_save_iter 10000 --max_iter 7e5 --print_iter 5000 \
+ --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
  --use_augment_dataloader --num_sim_factors 2 --augment_factor 2 \
  --name fVAE_k2_af2_seed3 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 100_000
@@ -31,7 +31,7 @@ nice python main.py --seed 4 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
  --output_save True --viz_on True --viz_port 4522 \
  --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 10000 --viz_ta_iter 10000 \
- --ckpt_save_iter 10000 --max_iter 7e5 --print_iter 5000 \
+ --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
  --use_augment_dataloader --num_sim_factors 2 --augment_factor 2 \
  --name fVAE_k2_af2_seed4 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 100_000
@@ -40,7 +40,7 @@ nice python main.py --seed 5 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
  --output_save True --viz_on True --viz_port 4522 \
  --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 10000 --viz_ta_iter 10000 \
- --ckpt_save_iter 10000 --max_iter 7e5 --print_iter 5000 \
+ --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
  --use_augment_dataloader --num_sim_factors 2 --augment_factor 2 \
  --name fVAE_k2_af2_seed5 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 100_000
