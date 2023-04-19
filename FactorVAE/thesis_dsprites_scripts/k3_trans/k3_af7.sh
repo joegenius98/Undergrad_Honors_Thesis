8a45e7c76410e610a82049b85c8a32569b51279b
Joseph S. Lee <jslee08@wm.edu>
2023-04-19 18:39:27 -0400

    Correct naming and input `--use_sort_strategy` for upcoming scripts

--- FactorVAE/thesis_dsprites_scripts/k3_trans/k3_af7.sh
+++ FactorVAE/thesis_dsprites_scripts/k3_trans/k3_af7.sh
@@ -6,8 +6,8 @@ nice python main.py --seed 1 \
  --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 50000 --viz_ta_iter 50000 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
- --use_augment_dataloader --num_sim_factors 3 --augment_choice 2 --augment_factor 7 \
- --name fVAE_k2_af2_seed1 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
+ --use_augment_dataloader --num_sim_factors 3 --use_sort_strategy --augment_choice 2 --augment_factor 7 \
+ --name trans_sort_k3_af7_seed1 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
 
 nice python main.py --seed 2 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
@@ -15,8 +15,8 @@ nice python main.py --seed 2 \
  --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 50000 --viz_ta_iter 50000 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
- --use_augment_dataloader --num_sim_factors 3 --augment_choice 2 --augment_factor 7 \
- --name fVAE_k2_af2_seed2 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
+ --use_augment_dataloader --num_sim_factors 3 --use_sort_strategy --augment_choice 2 --augment_factor 7 \
+ --name trans_sort_k3_af7_seed2 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
 
 nice python main.py --seed 3 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
@@ -24,8 +24,8 @@ nice python main.py --seed 3 \
  --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 50000 --viz_ta_iter 50000 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
- --use_augment_dataloader --num_sim_factors 3 --augment_choice 2 --augment_factor 7 \
- --name fVAE_k2_af2_seed3 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
+ --use_augment_dataloader --num_sim_factors 3 --use_sort_strategy --augment_choice 2 --augment_factor 7 \
+ --name trans_sort_k3_af7_seed3 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
 
 nice python main.py --seed 4 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
@@ -33,8 +33,8 @@ nice python main.py --seed 4 \
  --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 50000 --viz_ta_iter 50000 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
- --use_augment_dataloader --num_sim_factors 3 --augment_choice 2 --augment_factor 7 \
- --name fVAE_k2_af2_seed4 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
+ --use_augment_dataloader --num_sim_factors 3 --use_sort_strategy --augment_choice 2 --augment_factor 7 \
+ --name trans_sort_k3_af7_seed4 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
 
 nice python main.py --seed 5 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
@@ -42,5 +42,5 @@ nice python main.py --seed 5 \
  --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 50000 --viz_ta_iter 50000 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
- --use_augment_dataloader --num_sim_factors 3 --augment_choice 2 --augment_factor 7 \
- --name fVAE_k2_af2_seed5 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
+ --use_augment_dataloader --num_sim_factors 3 --use_sort_strategy --augment_choice 2 --augment_factor 7 \
+ --name trans_sort_k3_af7_seed5 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
