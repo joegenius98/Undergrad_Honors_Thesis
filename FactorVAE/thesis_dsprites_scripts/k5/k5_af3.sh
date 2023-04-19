a3b61f877d8075697f1eb7ec626098b2bbb8abc5
Joseph S. Lee <jslee08@wm.edu>
2023-04-19 08:54:06 -0400

    Change checkpoint freq. for k = 5

--- FactorVAE/thesis_dsprites_scripts/k5/k5_af3.sh
+++ FactorVAE/thesis_dsprites_scripts/k5/k5_af3.sh
@@ -7,7 +7,7 @@ nice python main.py --seed 1 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
  --use_augment_dataloader --num_sim_factors 5 --augment_factor 3 \
- --name fVAE_k2_af2_seed1 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 100_000
+ --name fVAE_k2_af2_seed1 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
 
 nice python main.py --seed 2 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
@@ -16,7 +16,7 @@ nice python main.py --seed 2 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
  --use_augment_dataloader --num_sim_factors 5 --augment_factor 3 \
- --name fVAE_k2_af2_seed2 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 100_000
+ --name fVAE_k2_af2_seed2 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
 
 nice python main.py --seed 3 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
@@ -25,7 +25,7 @@ nice python main.py --seed 3 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
  --use_augment_dataloader --num_sim_factors 5 --augment_factor 3 \
- --name fVAE_k2_af2_seed3 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 100_000
+ --name fVAE_k2_af2_seed3 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
 
 nice python main.py --seed 4 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
@@ -34,7 +34,7 @@ nice python main.py --seed 4 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
  --use_augment_dataloader --num_sim_factors 5 --augment_factor 3 \
- --name fVAE_k2_af2_seed4 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 100_000
+ --name fVAE_k2_af2_seed4 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
 
 nice python main.py --seed 5 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
@@ -43,4 +43,4 @@ nice python main.py --seed 5 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
  --use_augment_dataloader --num_sim_factors 5 --augment_factor 3 \
- --name fVAE_k2_af2_seed5 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 100_000
+ --name fVAE_k2_af2_seed5 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
