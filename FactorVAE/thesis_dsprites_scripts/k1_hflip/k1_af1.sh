1fa2452caccb069f8bafd72e45a0da3e31afb618
Joseph S. Lee <jslee08@wm.edu>
2023-04-21 16:02:18 -0400

    Write k = 1 scripts with horizontal flip

---
+++ FactorVAE/thesis_dsprites_scripts/k1_hflip/k1_af1.sh
@@ -0,0 +1,56 @@
+#! /bin/sh
+
+port=$1
+sh check_port.sh $port
+exit_status=$?
+
+if [ $exit_status -eq 1 ]
+then
+  exit 1
+fi
+
+
+nice python main.py --seed 1 \
+ --dataset dsprites --num_workers 4 --batch_size 64 \
+ --output_save True --viz_on True --viz_port $port \
+ --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 50000 --viz_ta_iter 50000 \
+ --max_iter 7e5 --print_iter 5000 \
+ --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
+ --use_augment_dataloader --augment_choice 3 --num_sim_factors 1 --augment_factor 1 \
+ --name fVAE_k1_seed1 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
+
+nice python main.py --seed 2 \
+ --dataset dsprites --num_workers 4 --batch_size 64 \
+ --output_save True --viz_on True --viz_port $port \
+ --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 50000 --viz_ta_iter 50000 \
+ --max_iter 7e5 --print_iter 5000 \
+ --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
+ --use_augment_dataloader --augment_choice 3 --num_sim_factors 1 --augment_factor 1 \
+ --name fVAE_k1_seed2 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
+
+nice python main.py --seed 3 \
+ --dataset dsprites --num_workers 4 --batch_size 64 \
+ --output_save True --viz_on True --viz_port $port \
+ --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 50000 --viz_ta_iter 50000 \
+ --max_iter 7e5 --print_iter 5000 \
+ --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
+ --use_augment_dataloader --augment_choice 3 --num_sim_factors 1 --augment_factor 1 \
+ --name fVAE_k1_seed3 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
+
+nice python main.py --seed 4 \
+ --dataset dsprites --num_workers 4 --batch_size 64 \
+ --output_save True --viz_on True --viz_port $port \
+ --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 50000 --viz_ta_iter 50000 \
+ --max_iter 7e5 --print_iter 5000 \
+ --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
+ --use_augment_dataloader --augment_choice 3 --num_sim_factors 1 --augment_factor 1 \
+ --name fVAE_k1_seed4 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
+
+nice python main.py --seed 5 \
+ --dataset dsprites --num_workers 4 --batch_size 64 \
+ --output_save True --viz_on True --viz_port $port \
+ --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 50000 --viz_ta_iter 50000 \
+ --max_iter 7e5 --print_iter 5000 \
+ --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
+ --use_augment_dataloader --augment_choice 3 --num_sim_factors 1 --augment_factor 1 \
+ --name fVAE_k1_seed5 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
