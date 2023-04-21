8beccd1daef0957fdf23e17235b624cb430e92b0
Joseph S. Lee <jslee08@wm.edu>
2023-04-21 18:33:05 -0400

    Refactor namings for `k5_hflip` folder's scripts

--- FactorVAE/thesis_dsprites_scripts/k5_hflip/k5_af3.sh
+++ FactorVAE/thesis_dsprites_scripts/k5_hflip/k5_af3.sh
@@ -17,7 +17,7 @@ nice python main.py --seed 1 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
  --use_augment_dataloader --augment_choice 3 --num_sim_factors 5 --augment_factor 3 \
- --name hflip_k2_af2_seed1 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
+ --name hflip_k5_af3_seed1 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
 
 nice python main.py --seed 2 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
@@ -26,7 +26,7 @@ nice python main.py --seed 2 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
  --use_augment_dataloader --augment_choice 3 --num_sim_factors 5 --augment_factor 3 \
- --name hflip_k2_af2_seed2 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
+ --name hflip_k5_af3_seed2 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
 
 nice python main.py --seed 3 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
@@ -35,7 +35,7 @@ nice python main.py --seed 3 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
  --use_augment_dataloader --augment_choice 3 --num_sim_factors 5 --augment_factor 3 \
- --name hflip_k2_af2_seed3 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
+ --name hflip_k5_af3_seed3 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
 
 nice python main.py --seed 4 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
@@ -44,7 +44,7 @@ nice python main.py --seed 4 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
  --use_augment_dataloader --augment_choice 3 --num_sim_factors 5 --augment_factor 3 \
- --name hflip_k2_af2_seed4 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
+ --name hflip_k5_af3_seed4 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
 
 nice python main.py --seed 5 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
@@ -53,4 +53,4 @@ nice python main.py --seed 5 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
  --use_augment_dataloader --augment_choice 3 --num_sim_factors 5 --augment_factor 3 \
- --name hflip_k2_af2_seed5 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
+ --name hflip_k5_af3_seed5 --z_dim 10 --gamma 10 --ckpt_load last --ckpt_save_iter 175_000
