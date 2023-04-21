48989f48dc0ce8e86e4f8c11b4af3406ee954b8b
Joseph S. Lee <jslee08@wm.edu>
2023-04-21 15:34:55 -0400

    Plug in `$port` parameter into all the scripts

--- FactorVAE/thesis_dsprites_scripts/k5/k5_af1.sh
+++ FactorVAE/thesis_dsprites_scripts/k5/k5_af1.sh
@@ -12,7 +12,7 @@ fi
 
 nice python main.py --seed 1 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
- --output_save True --viz_on True --viz_port 4516 \
+ --output_save True --viz_on True --viz_port $port \
  --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 50000 --viz_ta_iter 50000 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
@@ -21,7 +21,7 @@ nice python main.py --seed 1 \
 
 nice python main.py --seed 2 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
- --output_save True --viz_on True --viz_port 4516 \
+ --output_save True --viz_on True --viz_port $port \
  --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 50000 --viz_ta_iter 50000 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
@@ -30,7 +30,7 @@ nice python main.py --seed 2 \
 
 nice python main.py --seed 3 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
- --output_save True --viz_on True --viz_port 4516 \
+ --output_save True --viz_on True --viz_port $port \
  --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 50000 --viz_ta_iter 50000 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
@@ -39,7 +39,7 @@ nice python main.py --seed 3 \
 
 nice python main.py --seed 4 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
- --output_save True --viz_on True --viz_port 4516 \
+ --output_save True --viz_on True --viz_port $port \
  --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 50000 --viz_ta_iter 50000 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
@@ -48,7 +48,7 @@ nice python main.py --seed 4 \
 
 nice python main.py --seed 5 \
  --dataset dsprites --num_workers 4 --batch_size 64 \
- --output_save True --viz_on True --viz_port 4516 \
+ --output_save True --viz_on True --viz_port $port \
  --viz_ll_iter 1000 --viz_la_iter 5000 --viz_ra_iter 50000 --viz_ta_iter 50000 \
  --max_iter 7e5 --print_iter 5000 \
  --lr_VAE 1e-4 --beta1_VAE 0.9 --beta2_VAE 0.999 --lr_D 1e-4 --beta1_D 0.5 --beta2_D 0.9 \
